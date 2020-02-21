793727099589e82e294fc6b1d58e576b67bf03f8
angre-kim <angre@daum.net>
2020-02-21 15:47:10 +0900

    $time

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:clima/utilities/constants.dart';
+import 'package:clima/services/networking.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -26,30 +28,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     latitude = location.latitude;
     longitude = location.longitude;
-    getData();
-  }
 
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    if (response.statusCode == 200) {
-      String data = response.body;
 
-      var decodedDate = jsonDecode(data);
-      int condition = decodedDate['weather'][0]['id'];
-      double temperature = decodedDate['main']['temp'];
-      String cityName = decodedDate['name'];
+    var weatherData = await networkHelper.getData();
 
-      print(condition);
-      print(temperature);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen(locationWeather: weatherData,);
+
+    }));
+
+//    int condition = decodedDate['weather'][0]['id'];
+//    double temperature = decodedDate['main']['temp'];
+//    String cityName = decodedDate['name'];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center());
+    return Scaffold(
+        body: Center(
+          child: SpinKitDoubleBounce(
+            color: Colors.white,
+              size: 100.0,
+          ),
+
+        ));
   }
 }
